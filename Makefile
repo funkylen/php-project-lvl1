6843ed62eeb72b7f154e84beb62f3db14d402ba6
Vladlen Gilyazetdinov <v.gilyazetdinov@myhart.ru>
2021-10-06 14:05:09 +0300

    [4] Lint fix

--- Makefile
+++ Makefile
@@ -4,5 +4,11 @@ install:
 validate:
 	composer validate
 
+lint:
+	composer exec --verbose phpcs -- --standard=PSR12 src bin 
+
+lint-fix:
+	composer exec --verbose phpcbf -- --standard=PSR12 src bin 
+
 brain-games:
 	./bin/brain-games
--- src/Cli.php
+++ src/Cli.php
@@ -7,11 +7,10 @@ use function cli\prompt;
 
 class Cli
 {
-    public function getNameAndGreetUser() 
+    public function getNameAndGreetUser()
     {
         line('Welcome to the Brain Games!');
         $name = prompt('May I have your name?');
         line("Hello, %s!", $name);
     }
 }
-
