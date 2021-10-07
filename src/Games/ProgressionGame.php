50d723bf223630481ecad27abb02eb627716015e
Vladlen Gilyazetdinov <v.gilyazetdinov@myhart.ru>
2021-10-07 19:21:54 +0300

    [8] Init Progression Files

---
+++ src/Games/ProgressionGame.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace BrainGames\Games\ProgressionGame;
+
+use function BrainGames\Engine\startGame;
+
+function start($playerName)
+{
+    startGame($playerName, getDescription(), fn () => getQuestion());
+}
+
+function getDescription()
+{
+    return 'Find the greatest common divisor of given numbers.';
+}
+
+function getQuestion()
+{
+    $a = rand(0, 100);
+    $b = rand(0, 100);
+
+    return [
+        'content' => "$a $b",
+        'answer' => (string) gcd($a, $b),
+    ];
+}
+
+function gcd($a, $b)
+{
+    return ($a % $b) ? gcd($b, $a % $b) : $b;
+}
