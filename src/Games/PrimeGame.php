7701dc2acd414780f59624532212f2d9c2d547af
funkylen <funkylen@gmail.com>
2021-10-08 16:46:36 +0300

    [9] Init brain-prime files

---
+++ src/Games/PrimeGame.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace BrainGames\Games\PrimeGame;
+
+use function BrainGames\Engine\startGame;
+
+function start($playerName)
+{
+    startGame($playerName, getDescription(), fn () => getQuestion());
+}
+
+function getDescription()
+{
+    return 'What number is missing in the progression?';
+}
+
+function getQuestion()
+{
+    $base = rand(0, 100);
+    $step = rand(0, 100);
+    $maxLength = rand(5, 15);
+
+    $progression = [];
+    for ($i = 0; $i < $maxLength; $i++) {
+        $progression[] = $base + $step * $i;
+    }
+
+    $unknownItemKey = array_rand($progression);
+    $unknownItem = $progression[$unknownItemKey];
+    $progression[$unknownItemKey] = '..';
+
+    return [
+        'content' => implode(' ', $progression),
+        'answer' => (string) $unknownItem,
+    ];
+}
